a1bfb1be53afd3d8c2b9db69084145425e9eec7d
mmishra <mmishra@calibo.com>
2022-03-24 23:51:54 +0530

    v1-fix

--- packages/web/src/views/question/index.js
+++ packages/web/src/views/question/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BLNavbar } from "@spammetwice/common/src/components/navbar-before-login";
-const Question = () => {
-    const { user, accessToken, refreshToken, tenantId } = getSessionData();
+import { useNavigate } from "react-router-dom";
+import { auth } from "@spammetwice/auth";
 
-        if (!user || !accessToken || !refreshToken || !tenantId) {
-          historyHook("/login");
-          return;
-        }
-    return (
-        <div>
-            <BLNavbar/>
-        </div>
-    );
+const Question = () => {
+  const { user, accessToken, refreshToken, tenantId } = getSessionData();
+  const historyHook = useNavigate();
+  const { getSessionData } = auth();
+  if (!user || !accessToken || !refreshToken || !tenantId) {
+    historyHook("/login");
+    return;
+  }
+  return (
+    <div>
+      <BLNavbar />
+    </div>
+  );
 };
 
 export default Question;
